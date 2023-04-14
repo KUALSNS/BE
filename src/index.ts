0658c09382ab962c30ab8ffe2a30117937fbdf28
chrisheo <jftj@naver.com>
2023-04-14 23:13:18 +0900

    [init] 프로젝트 초기 세팅

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const index = express();
+const port = process.env.PORT || 3000;
+
+index.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+index.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
