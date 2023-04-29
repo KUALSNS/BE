7e87155de46990e0ac49c84837dce9f1623d54ae
seungryeol <opop0421@naver.com>
2023-04-29 11:29:18 +0900

    [feat] SQL db 연결

---
+++ src/config/auth.ts
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const env = process.env;
+
+const DATA_SOURCES = {
+  development: {
+    host: env.HOST!,
+    user: env.USER!,
+    password: env.PASSWORD!,
+    port: 3306,
+    database: env.DATABASE!,
+  }
+};
+
+export { DATA_SOURCES } 
