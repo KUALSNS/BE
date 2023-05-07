3b0ffcc538485f785c60c5209fb04eac8a1bb5ec
seungryeol <opop0421@naver.com>
2023-05-08 04:00:17 +0900

    [fix] refresh redis 연결 설정7

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -61,13 +61,16 @@ const refreshVerify = async (token: string, userId: number) => {
     if (typeof data === 'string') {
       if (token === data.split('Bearer ')[1]) {
           jwt.verify(data.split('Bearer ')[1], secret);
+          await redisClient.disconnect();
           return { state: true };
       } else {
+        await redisClient.disconnect();
         return { state: false };
       }
     }
-    await redisClient.disconnect();
+  
   } catch (err) {
+    await redisClient.disconnect();
     return { state: false };
   } finally {
    
