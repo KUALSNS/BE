8cdd9832b4e872c97feeee6fddf90662daf56c4a
seungryeol <opop0421@naver.com>
2023-05-09 02:30:50 +0900

    [fix] refresh redis 연결 설정12

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -15,27 +15,31 @@ const sign = (userId: string, userRole: number) => {
     id: userId,
     role: userRole,
   };
-
   return jwt.sign(payload, secret, {
     algorithm: 'HS256',
-    expiresIn: '2m',
+    expiresIn: '1m',
   });
 }
 
 const refresh = () => {
   return jwt.sign({}, secret, {
     algorithm: 'HS256',
-    expiresIn: '4m',
+    expiresIn: '2m',
   });
 }
 const decode = (token: string) => {
-
-  const decoded = jwt.decode(token) as JwtPayload;
-  return {
-    message: "Ok",
-    id: decoded!.id,
-    role: decoded!.role,
+  try {
+    const decoded = jwt.decode(token, { complete: true }) as JwtPayload;
+    return {
+      message: "Ok",
+      id: decoded.payload.id,
+      role: decoded.payload.role,
+    }
   }
+  catch (err) {
+    console.log(err);
+  }
+
 }
 
 const verify = (token: string) => {
@@ -43,6 +47,7 @@ const verify = (token: string) => {
   try {
     const decoded = jwt.verify(token, secret) as JwtPayload;
     return {
+      state: true,
       id: decoded!.id,
       role: decoded!.role,
     };
@@ -51,36 +56,27 @@ const verify = (token: string) => {
       state: false,
     };
   }
-}
+};
 
 const refreshVerify = async (token: string, userId: number) => {
- 
+  try {
     await redisClient.connect();
-    console.log(1);
     const data: string = await redisClient.v4.get(String(userId));
     console.log(data);
-   
-    try {
-    console.log(3);
     if (typeof data === 'string') {
-      console.log(7);
       if (token === data.split('Bearer ')[1]) {
-          jwt.verify(data.split('Bearer ')[1], secret);
-          console.log(4);
-          await redisClient.disconnect();
-          return { state: true };
+        jwt.verify(data.split('Bearer ')[1], secret);
+        return { state: true };
       } else {
-        console.log(5);
-        await redisClient.disconnect();
         return { state: false };
       }
-    } 
+    }
   } catch (err) {
-    console.log(6);
-    await redisClient.disconnect();
     return { state: false };
+  } finally {
+    await redisClient.disconnect();
   }
-}
+};
 
 
 
--- src/services/userService.ts
+++ src/services/userService.ts
@@ -9,17 +9,17 @@ const prisma = new PrismaClient();
 
 
 
-const userIdentifierSelect =  async ( userIdentifier: string) => {
+const userIdentifierSelect = async (userIdentifier: string) => {
   const connection = await mysql.createConnection(DATA_SOURCES.development);
   await connection.connect();
   try {
-    const  userSelect : string = `select user_id, role, password from users where identifier = '${String(userIdentifier)}'; `;
-    const userSelectResult : any  = await connection.query(userSelect);
+    const userSelect: string = `select user_id, role, password from users where identifier = '${String(userIdentifier)}'; `;
+    const userSelectResult: any = await connection.query(userSelect);
     return userSelectResult[0][0];
   } catch (error) {
     console.log(error);
   } finally {
-     await connection.end();
+    await connection.end();
   }
 }
 
@@ -28,7 +28,7 @@ const signUpService = async (
   email: string,
   password: string,
   nickname: string,
-  userId : string,
+  userId: string,
   phoneNumber: string
 ) => {
   const returnForm: serviceReturnForm = {
@@ -59,7 +59,7 @@ const signUpService = async (
     const TOKEN_KEY = process.env.TOKEN_KEY || "";
 
     // * Encrypt user password
-    let encryptedPassword = await bcrypt.hash(password, parseInt( "10"));
+    let encryptedPassword = await bcrypt.hash(password, parseInt("10"));
     // console.log(email)
     // const token = jwt.sign({ email }, TOKEN_KEY, {
     //   expiresIn: "20h",
@@ -70,7 +70,7 @@ const signUpService = async (
         //user_id: userId,
         nickname: nickname,
         email: email,
-        password:encryptedPassword,
+        password: encryptedPassword,
         phone: phoneNumber,
         role: "USER",
         identifier: userId,
@@ -92,5 +92,5 @@ const signUpService = async (
 
 
 
-export  { userIdentifierSelect,  signUpService }
+export { userIdentifierSelect, signUpService }
 
