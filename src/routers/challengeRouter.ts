ff986929ed7552389c444988bed2dd16ed2aa52a
seungryeol <opop0421@naver.com>
2023-05-13 00:19:27 +0900

    [add] 글쓰기 페이지 관련 API 파일 이전

--- src/routers/challengeRouter.ts
+++ src/routers/challengeRouter.ts
@@ -1,14 +1,14 @@
 import express from 'express';
 import  * as ChallengeController  from '../controllers/challengeController'
-
+import  * as WriteController  from '../controllers/writeController'
 import  {verifyToken} from '../middleware/auth';
 
 
 
-const router = express.Router()
+const router = express.Router();
 
 router.get('/', ChallengeController.beforeMain);
-router.post('/start/:name', verifyToken, ChallengeController.newChallenge);
+router.post('/start/:name', verifyToken, WriteController.newChallenge);
 router.get('/main', verifyToken ,ChallengeController.afterMain);
 router.get('/whole-category', ChallengeController.wholeCategory);
 router.get('/select', ChallengeController.manyCategory);
