fd8b3d0da0d693b546e786d602bd964cc304d246
seungryeol <opop0421@naver.com>
2023-05-14 02:47:53 +0900

    [fix] 로그인 후 메인페이지 API 조건 수정

--- src/services/challengeService.ts
+++ src/services/challengeService.ts
@@ -263,6 +263,9 @@ const afterMainData = async (user_id: number) => {
             }
           },
           user_challenge_templetes: {
+            where:{
+              complete:true
+            },
             select: {
               title: true
             }
@@ -316,180 +319,180 @@ const afterMainData = async (user_id: number) => {
   }
 }
 
-// const newChallengeData = async (user_id: number, newChallenge: string) => {
-//   try {
-//     const challengTemplateDB = await prisma.user_challenge_templetes.findMany({
+const newChallengeData = async (user_id: number, newChallenge: string) => {
+  try {
+    const challengTemplateDB = await prisma.user_challenge_templetes.findMany({
 
-//       select: {
-//         created_at: true
-//       }
+      select: {
+        created_at: true
+      }
 
-//     });
-//     console.log(challengTemplateDB[0].created_at)
-//     const [userCooponDB, challengesCountDB, challengesOverlapDB] = await Promise.all([
-//       await prisma.users.findUnique({
-//         where: {
-//           user_id: user_id
-//         },
-//         select: {
-//           coopon: true
-//         }
-//       }),
-//       await prisma.user_challenges.aggregate({
-//         where: {
-//           user_id: user_id
-//         },
-//         _count: {
-//           uchal_id: true
-//         }
-//       }),
-//       await prisma.challenges.findMany({
-//         where: {
-//           title: newChallenge
-//         },
-//         select: {
-//           user_challenges: {
-//             select: {
-//               chal_id: true
-//             }
-//           }
-//         }
-//       })
-//     ])
+    });
+    console.log(challengTemplateDB[0].created_at)
+    const [userCooponDB, challengesCountDB, challengesOverlapDB] = await Promise.all([
+      await prisma.users.findUnique({
+        where: {
+          user_id: user_id
+        },
+        select: {
+          coopon: true
+        }
+      }),
+      await prisma.user_challenges.aggregate({
+        where: {
+          user_id: user_id
+        },
+        _count: {
+          uchal_id: true
+        }
+      }),
+      await prisma.challenges.findMany({
+        where: {
+          title: newChallenge
+        },
+        select: {
+          user_challenges: {
+            select: {
+              chal_id: true
+            }
+          }
+        }
+      })
+    ])
 
-//     const coopon = userCooponDB?.coopon;
-//     const challengesCount = challengesCountDB._count.uchal_id;
-//     const challengesOverlap = challengesOverlapDB[0].user_challenges[0];
-//     prisma.$disconnect();
-//     return {
-//       coopon,
-//       challengesCount,
-//       challengesOverlap
-//     }
+    const coopon = userCooponDB?.coopon;
+    const challengesCount = challengesCountDB._count.uchal_id;
+    const challengesOverlap = challengesOverlapDB[0].user_challenges[0];
+    prisma.$disconnect();
+    return {
+      coopon,
+      challengesCount,
+      challengesOverlap
+    }
 
-//   } catch (error) {
-//     prisma.$disconnect();
-//     console.log(error);
-//   }
-// };
+  } catch (error) {
+    prisma.$disconnect();
+    console.log(error);
+  }
+};
 
-// const startChallengeData = async (user_id: number, newChallenge: string) => {
-//   try {
-//     const chalId = await prisma.challenges.findMany({
-//       where: {
-//         title: newChallenge
-//       },
-//       select: {
-//         chal_id: true
+const startChallengeData = async (user_id: number, newChallenge: string) => {
+  try {
+    const chalId = await prisma.challenges.findMany({
+      where: {
+        title: newChallenge
+      },
+      select: {
+        chal_id: true
 
-//       }
-//     });
-//     const chalIdData = chalId[0].chal_id;
-//     await prisma.user_challenges.create({
-//       data: {
-//         user_id: user_id,
-//         chal_id: chalIdData
-//       }
-//     });
+      }
+    });
+    const chalIdData = chalId[0].chal_id;
+    await prisma.user_challenges.create({
+      data: {
+        user_id: user_id,
+        chal_id: chalIdData
+      }
+    });
 
-//     prisma.$disconnect();
-//     return { chalIdData, newChallenge };
+    prisma.$disconnect();
+    return { chalIdData, newChallenge };
 
-//   } catch (error) {
-//     console.log(error);
-//     prisma.$disconnect();
-//     return false;
-//   }
-// };
+  } catch (error) {
+    console.log(error);
+    prisma.$disconnect();
+    return false;
+  }
+};
 
-// const newChallengeResult = async (user_id: number, challenge_id: number, newChallenge: string) => {
-//   try {
-//     const date = new Date().toLocaleString('en-US', { timeZone: 'Asia/Seoul' });
-//     const isoDate = new Date(date).toISOString().slice(0, 10) + "T00:00:00.000Z";
-//     const realDate = new Date(isoDate);
-//     console.log(realDate);
-//     const challengTemplateArray: {
-//       challenge: string;
-//       category: string;
-//       "template-title": string;
-//       "template-content": string;
-//     }[] = [];
-//     const relativeChallengeArray = [];
+const newChallengeResult = async (user_id: number, challenge_id: number, newChallenge: string) => {
+  try {
+    const date = new Date().toLocaleString('en-US', { timeZone: 'Asia/Seoul' });
+    const isoDate = new Date(date).toISOString().slice(0, 10) + "T00:00:00.000Z";
+    const realDate = new Date(isoDate);
+    console.log(realDate);
+    const challengTemplateArray: {
+      challenge: string;
+      category: string;
+      "template-title": string;
+      "template-content": string;
+    }[] = [];
+    const relativeChallengeArray = [];
 
-//     const [challengTemplateDB, relativeChallengeDB] = await Promise.all([
-//       await prisma.challenges.findMany({
-//         where: {
-//           chal_id: challenge_id
-//         },
-//         select: {
-//           title: true,
-//           category: {
-//             select: {
-//               name: true
-//             },
-//           },
-//           templates: {
-//             select: {
-//               title: true,
-//               content: true
-//             }
-//           }
-//         }
-//       }),
-//       await prisma.user_challenges.findMany({
-//         where: {
-//           user_id: user_id
-//         },
-//         select: {
-//           challenges: {
-//             select: {
-//               title: true
-//             }
-//           },
-//           user_challenge_templetes: {
-//             where: {
-//               created_at: realDate,
-//             },
-//             select: {
-//               uctem_id: true
-//             }
-//           }
-//         }
-//       })
-//     ]);
-//     for (var i = 0; i < challengTemplateDB[0].templates.length; i++) {
-//       const challengTemplate = challengTemplateDB.map((e) => {
-//         return [{
-//           "challenge": e.title, "category": e.category.name,
-//           "template-title": e.templates[i].title,
-//           "template-content": e.templates[i].content
-//         }]
-//       });
-//       challengTemplateArray.push(challengTemplate[0][0]);
-//     }
+    const [challengTemplateDB, relativeChallengeDB] = await Promise.all([
+      await prisma.challenges.findMany({
+        where: {
+          chal_id: challenge_id
+        },
+        select: {
+          title: true,
+          category: {
+            select: {
+              name: true
+            },
+          },
+          templates: {
+            select: {
+              title: true,
+              content: true
+            }
+          }
+        }
+      }),
+      await prisma.user_challenges.findMany({
+        where: {
+          user_id: user_id
+        },
+        select: {
+          challenges: {
+            select: {
+              title: true
+            }
+          },
+          user_challenge_templetes: {
+            where: {
+              created_at: realDate,
+            },
+            select: {
+              uctem_id: true
+            }
+          }
+        }
+      })
+    ]);
+    for (var i = 0; i < challengTemplateDB[0].templates.length; i++) {
+      const challengTemplate = challengTemplateDB.map((e) => {
+        return [{
+          "challenge": e.title, "category": e.category.name,
+          "template-title": e.templates[i].title,
+          "template-content": e.templates[i].content
+        }]
+      });
+      challengTemplateArray.push(challengTemplate[0][0]);
+    }
 
-//     for (var i = 0; i < relativeChallengeDB.length; i++) {
-//       if (!relativeChallengeDB[i].user_challenge_templetes) {
-//         const relativeChallengeMap = relativeChallengeDB.map((e) => {
-//           return e.challenges;
-//         });
-//         relativeChallengeArray.push(relativeChallengeMap[i].title);
-//       }
-//     }
+    for (var i = 0; i < relativeChallengeDB.length; i++) {
+      if (!relativeChallengeDB[i].user_challenge_templetes) {
+        const relativeChallengeMap = relativeChallengeDB.map((e) => {
+          return e.challenges;
+        });
+        relativeChallengeArray.push(relativeChallengeMap[i].title);
+      }
+    }
 
-//     const valueFilter = relativeChallengeArray.filter((element) => element !== newChallenge);
-//     valueFilter.unshift(newChallenge);
-//     prisma.$disconnect();
-//     return { valueFilter, challengTemplateArray };
-//   } catch (error) {
-//     console.log(error);
-//     prisma.$disconnect();
-//   }
-// };
+    const valueFilter = relativeChallengeArray.filter((element) => element !== newChallenge);
+    valueFilter.unshift(newChallenge);
+    prisma.$disconnect();
+    return { valueFilter, challengTemplateArray };
+  } catch (error) {
+    console.log(error);
+    prisma.$disconnect();
+  }
+};
 
 export {
   beforeMainData, wholeCategoryData, oneCategoryData,
   manyCategoryData, challengeSearchData, afterMainData,
- // newChallengeData, startChallengeData, newChallengeResult
+  newChallengeData, startChallengeData, newChallengeResult
 
 }
