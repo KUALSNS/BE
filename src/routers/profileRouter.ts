127da1bf20645f12f1cdb25723daaf8e8d1721a4
chrisheo <jftj@naver.com>
2023-05-18 20:14:28 +0900

    [fix] 프로필 라우터 verify token 추가

--- src/routers/profileRouter.ts
+++ src/routers/profileRouter.ts
@@ -3,16 +3,17 @@ import cors from 'cors';
 const router = express.Router()
 import * as ProfileController  from '../controllers/profileController'
 import {passwordUpdate, profileUpdate, userChallengeStatistics} from '../controllers/profileController';
+import { verifyToken } from '../middleware/auth';
 
 router.use(cors({
   credentials : true
 }));
 
-router.get('/', ProfileController.userProfile);
-router.patch('/', ProfileController.profileUpdate);
-router.get('/history', ProfileController.userChallengeStatistics);
-router.patch('/password', ProfileController.passwordUpdate);
-router.patch('/phone', ProfileController.profileUpdate);
-router.patch('/email', ProfileController.emailUpdate);
+router.get('/', verifyToken, ProfileController.userProfile);
+router.patch('/', verifyToken, ProfileController.profileUpdate);
+router.get('/history', verifyToken, ProfileController.userChallengeStatistics);
+router.patch('/password', verifyToken, ProfileController.passwordUpdate);
+router.patch('/phone', verifyToken, ProfileController.profileUpdate);
+router.patch('/email', verifyToken, ProfileController.emailUpdate);
 
 export = router
