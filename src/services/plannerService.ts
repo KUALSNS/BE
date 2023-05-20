430bdecd633fd057fa4132277f3038c8555d33d9
chrisheo <jftj@naver.com>
2023-05-20 12:36:05 +0900

    [fix] 카테고리 정보 추가

--- src/services/plannerService.ts
+++ src/services/plannerService.ts
@@ -55,12 +55,15 @@ export async function getUserChallengeHistory(userId: number) {
             include: {
                 user_challenges: {
                     include: {
-                        challenges: true,
+                        challenges: {
+                            include: {
+                                category: true,
+                            }
+                        },
                         user_challenge_templetes: {
                             orderBy: {update_at: 'desc'},
                             //take: 1,
                         },
-                        //user_challenge_templetes: true,
                     },
                 },
             },
@@ -114,48 +117,5 @@ export async function getUserChallengeHistory(userId: number) {
     }
 }
 
-
-
-
-export async function gsetUserChallengeHistory(userId: number) {
-    const user = await prisma.users.findUnique({
-        where: {user_id: userId},
-        include: {
-            user_challenges: {
-                include: {
-                    challenges: true,
-                    user_challenge_templetes: {
-                        orderBy: {update_at: 'desc'},
-                        take: 1,
-                    },
-                },
-            },
-        },
-    });
-
-    console.log(user)
-    // ongoingChallenges, completedChallenges, temporarilySavedChallenges filter
-    const ongoingChallenges = user?.user_challenges.filter(
-      // 시작했고, 끝나지 않은 챌린지
-      (challenge) => !challenge.complete && challenge.finish_at === null
-    );
-
-    const finishedChallenges = user?.user_challenges.filter(
-      // 끝난 챌린지
-      (challenge) => challenge.complete
-    );
-
-    const temporarilySavedChallenges = user?.user_challenges.filter(
-      //임시저장 챌린지
-      (challenge) => !challenge.complete && challenge.finish_at !== null
-    );
-
-
-    return {
-        ongoingChallenges,
-        finishedChallenges,
-        temporarilySavedChallenges,
-    };
-}
 
 
