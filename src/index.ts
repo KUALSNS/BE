8244c75647c721cf9ba4bc73bb1d250374fe511f
seungryeol <opop0421@naver.com>
2023-07-05 13:10:43 +0900

    [fix] 무중단 배포 시도(컨테이너 별 포트 추가)

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,9 @@ import {stream} from './modules/loggerHandler';
 import {getKoreanDateISOString, getKoreanDateISOStringAdd9Hours} from './modules/koreanTime';
 dotenv.config();
 const app = express();
-const port = process.env.PORT1 || process.env.PORT2 || 3000;
 
+const port1 = process.env.PORT1 || 3000;
+const port2 = process.env.PORT2 || 3001;
 
   const koreanDateISOString = getKoreanDateISOString();
   const koreanTime = new Date(koreanDateISOString)
@@ -39,15 +40,26 @@ app.use('/api/planner', require('./routers/plannerRouter'));
 
 
 app
-    .listen(port, () => {
+    .listen(port1, () => {
         console.log(`
     ################################################
-          🛡️  Server listening on port: ${port} 🛡️
+          🛡️  Server listening on port: ${port1} 🛡️
     ################################################
   `);
         console.info('Tarae Server Start');
     })
     .on('error', (err) => {
+        console.error(err);
+        process.exit(1);
+    });
+
+    app.listen(port2, () => {
+        console.log(`
+          ################################################
+                🛡️  Server 2 listening on port: ${port2} 🛡️
+          ################################################
+        `);
+      }).on('error', (err) => {
         console.error(err);
         process.exit(1);
     });
