0655b317297d5c36b1189676fc5406872ca2f757
seungryeol <opop0421@naver.com>
2023-09-25 01:06:00 +0900

    [fix]  재발급 수정

--- src/modules/jwtModules.ts
+++ src/modules/jwtModules.ts
@@ -65,18 +65,17 @@ const refreshVerify = async (token: string, userId: number) => {
   
       jwt.verify(data.split('Bearer ')[1], secret);
    
-      //await redisClient.disconnect();
-     
+      await redisClient.disconnect();
       return { state: true };
     }
-    //await redisClient.disconnect();
+    await redisClient.disconnect();
     return { state: false };
 
   } catch (err) {
-   // await redisClient.disconnect();
+    await redisClient.disconnect();
     return { state: false };
   }finally{
-     await redisClient.disconnect(); // 연결 종료
+  //   await redisClient.disconnect(); // 연결 종료
   }
 };
 
