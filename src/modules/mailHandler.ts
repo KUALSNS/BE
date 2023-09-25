c95311006000e375060913301444b906430a4127
seungryeol <opop0421@naver.com>
2023-09-25 20:59:58 +0900

    [fix] 인증메일 제목,내용 변경

--- src/modules/mailHandler.ts
+++ src/modules/mailHandler.ts
@@ -32,16 +32,22 @@ export const smtpSender = async function (email: string) {
     const mailOptions = {
       from: "teamwritoner" + "@gmail.com",
       to: email,
-      subject: 'Writon 서비스 이메일 인증 코드입니다.',
-      text: `인증 코드 : ${verificationCode}`
+      subject: '[Writon] 요청하신 서비스 이메일 인증 번호를 안내해드립니다.',
+      html: `
+      <p>본 메일은 Writon 서비스의 회원가입을 위한 이메일 인증입니다.</p>
+    
+      <p>진행 중인 화면에 인증번호를 정확히 입력해주세요.</p>
+    
+      <p>인증번호는 메일 발송 시점부터 3분 동안 유효합니다.</p>
+    
+      <p><strong>** 인증 코드 **</strong> : ${verificationCode}</p>
+      `
     };
     const auth = await smtpTransport.sendMail(mailOptions);
     smtpTransport.close();
     console.log("here is email and auth")
     console.log(email + "       " + auth);
-   // await redisClient.disconnect();
    
-    
     return {
       status: 200,
       message: '이메일이 성공적으로 전송되었습니다.',
